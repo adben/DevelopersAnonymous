169badcd658d4af3732ac2cd65329e8154ce2da0
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2013-03-01 15:31:11 +0100

    some constants

--- java/src/main/java/org/joyofcoding/objectcalisthenics/GildedRose.java
+++ java/src/main/java/org/joyofcoding/objectcalisthenics/GildedRose.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class GildedRose {
 
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
+    private static final String DEXTERITY_VEST = "+5 Dexterity Vest";
+    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+
     public static void main(String[] args) {
         GildedRose gildedRose = new GildedRose();
         gildedRose.updateQuality(gildedRose.makeItems());
@@ -12,24 +19,24 @@ public class GildedRose {
 
     public List<Item> makeItems() {
         List<Item> items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+        items.add(new Item(DEXTERITY_VEST, 10, 20));
+        items.add(new Item(AGED_BRIE, 2, 0));
+        items.add(new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7));
+        items.add(new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80));
+        items.add(new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT, 15, 20));
+        items.add(new Item(CONJURED_MANA_CAKE, 3, 6));
         return items;
     }
 
     public void updateQuality(List<Item> list) {
         List<Item> items = list;
         for (int i = 0; i < items.size(); i++) {
-            if (!"Aged Brie".equals(items.get(i).getName())
-                    && !"Backstage passes to a TAFKAL80ETC concert".equals(items
+            if (!AGED_BRIE.equals(items.get(i).getName())
+                    && !BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT.equals(items
                     .get(i)
                     .getName())) {
                 if (items.get(i).getQuality() > 0) {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+                    if (!SULFURAS_HAND_OF_RAGNAROS.equals(items.get(i).getName())) {
                         Item.decreaseItemQuality(items, i);
                     }
                 }
@@ -37,7 +44,7 @@ public class GildedRose {
                 if (items.get(i).getQuality() < 50) {
                     Item.increaseItemQuality(items, i);
 
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items
+                    if (BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT.equals(items
                             .get(i)
                             .getName())) {
                         if (items.get(i).getSellIn() < 11) {
@@ -51,17 +58,17 @@ public class GildedRose {
                 }
             }
 
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+            if (!SULFURAS_HAND_OF_RAGNAROS.equals(items.get(i).getName())) {
                 items.get(i).setSellIn(items.get(i).getSellIn() - 1);
             }
 
             if (items.get(i).getSellIn() < 0) {
-                if (!items.get(i).getName().equals("Aged Brie")) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items
+                if (!items.get(i).getName().equals(AGED_BRIE)) {
+                    if (!BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT.equals(items
                             .get(i)
                             .getName())) {
                         if (items.get(i).getQuality() > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName())) {
+                            if (!SULFURAS_HAND_OF_RAGNAROS.equals(items.get(i).getName())) {
                                 Item.decreaseItemQuality(items, i);
                             }
                         }
